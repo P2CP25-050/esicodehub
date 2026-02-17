dbfee807dc08d65c7b3dd6f6c2f918eebeea8565
Dhia21k <doudouhouam@gmail.com>
2026-02-17 11:09:48 +0100

    feat(backend): add health check integration

--- backend/config/settings/base.py
+++ backend/config/settings/base.py
@@ -41,9 +41,17 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'corsheaders',
+    'core',
+]
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000",
+    "http://127.0.0.1:3000",
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
