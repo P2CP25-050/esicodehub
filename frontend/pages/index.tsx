9d609bbd142061531f7535a1964fa8a77be06c7f
Dhia21k <doudouhouam@gmail.com>
2026-03-06 16:01:56 +0100

    chore: add dockerization for the project

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -1,3 +1,4 @@
+// Health Check
 import { useEffect, useState } from 'react';
 import apiClient from '../lib/axios';
 
@@ -22,3 +23,26 @@ export default function Home() {
     </div>
   );
 }
+// Test Check (for REST-API)
+// 
+// const HomePage = () => {
+//   const [data, setData] = useState(null);
+// 
+//   useEffect(() => {
+//     // Fetch data from the backend using a GET request
+//     fetch("http://localhost:8000/api/health/")
+//       .then((res) => res.json())
+//       .then((data) => setData(data.data))
+//       .catch((error) => console.error('Error fetching data:', error));
+//   }, []); // Empty dependency array ensures this runs only once on component mount
+//   
+// return (
+//     <div>
+//       <h1>Dockerised Full-Stack Template</h1>
+//       <h3>With Django, React, Postgres, and Docker</h3>
+//       <p>{data ? data : "Loading data..."}</p> {/* Display data or loading message */}
+//     </div>
+//   );
+// };
+// 
+// export default HomePage;
