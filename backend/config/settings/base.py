e9b0a47f90267b75678be5d04d056f3d31911f1e
AbderrahmaneTB <abdrahmanetb@gmail.com>
2026-03-07 15:29:20 +0100

    feat(auth): add custom user model

--- backend/config/settings/base.py
+++ backend/config/settings/base.py
@@ -138,3 +138,16 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
+
+from datetime import timedelta
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ),
+}
+
+SIMPLE_JWT = {
+    'ACCESS_TOKEN_LIFETIME':  timedelta(minutes=60),
+    'REFRESH_TOKEN_LIFETIME': timedelta(days=7),
+}
