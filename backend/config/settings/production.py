bb41faecd72d5c03fa3492410581d87e067cfbfe
Dhia21k <doudouhouam@gmail.com>
2026-03-07 03:05:06 +0100

    fix(ci): fix db host

--- backend/config/settings/production.py
+++ backend/config/settings/production.py
@@ -1,7 +1,7 @@
-## Fill once you start production
+# Fill once you start production
 
-## from .base import *
-## 
-## DEBUG = False
-## ALLOWED_HOSTS = ['your-domain.com']
-## # Use PostgreSQL, read from environment variables
+# from .base import *
+
+# DEBUG = False
+# ALLOWED_HOSTS = ['your-domain.com']
+# Use PostgreSQL, read from environment variables
