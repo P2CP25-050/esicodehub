53a9e227e8dbbf13523f765cd9873c57b82e0085
Dhia21k <doudouhouam@gmail.com>
2026-03-07 03:05:06 +0100

    fix(ci): fix db host

--- backend/apps/core/views.py
+++ backend/apps/core/views.py
@@ -1,12 +1,13 @@
-from django.shortcuts import render
+# from django.shortcuts import render
 from django.http import JsonResponse
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+
 
 # Create your views here.
 def health_check(request):
     return JsonResponse({"status": "ok", "message": "Django is running"})
 
-from rest_framework.decorators import api_view
-from rest_framework.response import Response
 
 @api_view(['GET'])
 def send_test_data(request):
