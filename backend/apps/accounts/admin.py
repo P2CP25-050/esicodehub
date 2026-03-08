b7a0e3f0e95eec2f0e4b05d9279dcbe8dfb932c8
AbderrahmaneTB <abdrahmanetb@gmail.com>
2026-03-08 18:37:25 +0100

    feat(auth): add custom user model
    * feat(auth): add custom user model
    
    * feat(auth): Update custom user model
    
    * feat(auth): finalize User model
    
    ---------
    
    Co-authored-by: Dhia21k <od_houam@esi.dz>

--- backend/apps/accounts/admin.py
+++ backend/apps/accounts/admin.py
@@ -1,5 +1,20 @@
 from django.contrib import admin
-from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from .models import User
 
-admin.site.register(User, UserAdmin)
+
+@admin.register(User)
+class UserAdmin(BaseUserAdmin):
+    ordering = ['email']
+    list_display = ['email', 'first_name', 'last_name', 'role', 'is_verified', 'is_active']
+    fieldsets = (
+        (None, {'fields': ('email', 'password')}),
+        ('Personal info', {'fields': ('first_name', 'last_name', 'role', 'school_id')}),
+        ('Permissions', {'fields': ('is_active', 'is_verified', 'is_staff', 'is_superuser')}),
+    )
+    add_fieldsets = (
+            (None, {
+                'classes': ('wide',),
+                'fields': ('email', 'password1', 'password2', 'role'),
+            }),
+    )
