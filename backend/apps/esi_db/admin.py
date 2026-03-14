52f76b5bef5c4449d09062ae3a62a5459a76a30d
Dhia21k <doudouhouam@gmail.com>
2026-03-14 01:17:00 +0100

    fix(accounts): set is_verified to true for superusers and add group to display in admin panel

--- backend/apps/esi_db/admin.py
+++ backend/apps/esi_db/admin.py
@@ -4,7 +4,7 @@ from .models import EsiStudent, EsiProfessor
 
 @admin.register(EsiStudent)
 class EsiStudentAdmin(admin.ModelAdmin):
-    list_display = ['school_id', 'first_name', 'last_name', 'email', 'study_year', 'section', 'status']
+    list_display = ['school_id', 'first_name', 'last_name', 'email', 'study_year', 'section', 'group', 'status']
     list_filter = ['study_year', 'status', 'section']
     search_fields = ['first_name', 'last_name', 'email', 'school_id']
 
