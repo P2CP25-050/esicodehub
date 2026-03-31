2bc6a550e395b54b86bad8b024e3ed8bfbb5f9bd
Dhia Eddine HOUAM <86988369+Dhia21k@users.noreply.github.com>
2026-03-31 22:41:19 +0100

    Fix flake8 issues
    Added EmailBackend class to handle authentication using email.

--- backend/apps/accounts/backends.py
+++ backend/apps/accounts/backends.py
@@ -1,7 +1,9 @@
 from django.contrib.auth.backends import ModelBackend
 from django.contrib.auth import get_user_model
 
+
 User = get_user_model()
+
 
 class EmailBackend(ModelBackend):
     def authenticate(self, request, username=None, email=None, password=None, **kwargs):
