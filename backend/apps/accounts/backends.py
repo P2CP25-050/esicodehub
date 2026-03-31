f397114173821d82b3f5df3a4ec91f41b64ef677
AbderrahmaneTB <abdrahmanetb@gmail.com>
2026-03-31 21:35:42 +0100

    fix: email authentication backend and dockerfile

--- backend/apps/accounts/backends.py
+++ backend/apps/accounts/backends.py
@@ -1,22 +1,17 @@
 from django.contrib.auth.backends import ModelBackend
 from django.contrib.auth import get_user_model
 
-
 User = get_user_model()
 
-
 class EmailBackend(ModelBackend):
-    def authenticate(self, request, email=None, password=None, **kwargs):
-        """ Custom authentication backend that allows Django to authenticate
-         users using email instead of the default username field"""
+    def authenticate(self, request, username=None, email=None, password=None, **kwargs):
+        # Django admin passes email as 'username', handle both cases
+        email = email or username
         try:
             user = User.objects.get(email=email)
         except User.DoesNotExist:
-            # Email not found
             return None
 
         if user.check_password(password):
-            # Password matches
             return user
-        # Password is wrong
         return None
