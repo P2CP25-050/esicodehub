b8254de0d839c2ca6f164e242e6bfae1051c2832
AbderrahmaneTB <abdrahmanetb@gmail.com>
2026-04-03 08:43:14 +0100

    feat: implement assignment management REST API

---
+++ backend/apps/assignment_submissions/urls.py
@@ -0,0 +1,17 @@
+from django.urls import path
+
+from .views import AssignmentDetailView, AssignmentListCreateView
+
+
+urlpatterns = [
+    path(
+        '',
+        AssignmentListCreateView.as_view(),
+        name='assignment-list-create',
+    ),
+    path(
+        '<int:pk>/',
+        AssignmentDetailView.as_view(),
+        name='assignment-detail',
+    ),
+]
