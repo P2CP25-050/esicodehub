87762a1e7ed37d35df53edff13d45d7b558cc372
houssamdjaidja <oh_djaidja@esi.dz>
2026-05-01 01:51:19 +0100

    Update new.tsx

--- frontend/pages/submissions/new.tsx
+++ frontend/pages/submissions/new.tsx
@@ -1,4 +1,4 @@
-import { useState, CSSProperties, ChangeEvent, useEffect } from "react";
+import { useState, ChangeEvent, useEffect } from "react";
 import { useRouter } from "next/router";
 import axios from "axios";
 import Header from "@/components/submissions/Header";
