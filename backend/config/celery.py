cd011db7b4f569b1e49719bc04f7ae0e5f6209fc
Dhia21k <doudouhouam@gmail.com>
2026-05-05 00:33:38 +0100

    chore: get branch ready for deployment

--- backend/config/celery.py
+++ backend/config/celery.py
@@ -1,7 +1,10 @@
 import os
 from celery import Celery
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings.development')
+os.environ.setdefault(
+	'DJANGO_SETTINGS_MODULE',
+	os.getenv('DJANGO_SETTINGS_MODULE', 'config.settings.production')
+)
 
 app = Celery('esicodehub')
 app.config_from_object('django.conf:settings', namespace='CELERY')
